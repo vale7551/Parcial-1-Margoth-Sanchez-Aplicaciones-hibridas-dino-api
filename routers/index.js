16bfafcdb5837feb525b8822f06ed12b6985120d
vale7551 <margoth.sanchezordonez@davinci.edu.ar>
2025-09-25 23:58:35 -0300

    Delete routers directory

--- routers/index.js
+++
@@ -1,7 +0,0 @@
-import dinosauriosRouter from "./dinosaurios.js";
-import paleontologosRouter from "./paleontologos.js";
-
-export default (app) => {
-  app.use("/api/dinosaurios", dinosauriosRouter);
-  app.use("/api/paleontologos", paleontologosRouter);
-};
